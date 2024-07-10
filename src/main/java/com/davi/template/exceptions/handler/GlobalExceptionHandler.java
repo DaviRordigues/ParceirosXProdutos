ec8a71f0bd9084b5586d877743c5ea1f538dd84e
DaviRordigues <davirodriguesbezerra@gmail.com>
2024-07-10 12:06:01 -0300

    Exepitons implementadas

---
+++ src/main/java/com/davi/template/exceptions/handler/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.davi.template.exceptions.handler;
+
+import com.davi.template.exceptions.ProductNotFoundException;
+import com.davi.template.exceptions.SkuNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<String> handleProductNotFoundException(ProductNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(SkuNotFoundException.class)
+    public ResponseEntity<String> handleSkuNotFoundException(SkuNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
