0c720380ec1accedba986d37568942c9b008d62b
DaviRordigues <davirodriguesbezerra@gmail.com>
2024-07-10 13:19:01 -0300

    request corrigida

--- src/main/java/com/davi/template/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/davi/template/exceptions/handler/GlobalExceptionHandler.java
@@ -1,27 +1,43 @@
-package com.davi.template.exceptions.handler;
+package com.davi.template.exceptions;
 
-import com.davi.template.exceptions.ProductNotFoundException;
-import com.davi.template.exceptions.SkuNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<String> handleProductNotFoundException(ProductNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    @ExceptionHandler(SkuNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleSkuNotFoundException(SkuNotFoundException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("Error", "Sku Not Found");
+        errorResponse.put("Code", "SKU_NOT_FOUND");
+        errorResponse.put("Description", ex.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(SkuNotFoundException.class)
-    public ResponseEntity<String> handleSkuNotFoundException(SkuNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleProductNotFoundException(ProductNotFoundException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("Error", "Product Not Found");
+        errorResponse.put("Code", "PRODUCT_NOT_FOUND");
+        errorResponse.put("Description", ex.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGenericException(Exception ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<Map<String, Object>> handleGenericException(Exception ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("Error", "Internal Server Error");
+        errorResponse.put("Code", "INTERNAL_SERVER_ERROR");
+        errorResponse.put("Description", ex.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 }
