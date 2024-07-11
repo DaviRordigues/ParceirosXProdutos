e9e326acff2c6f91a5a62433b3535df434c83879
DaviRordigues <davirodriguesbezerra@gmail.com>
2024-07-11 10:42:12 -0300

    erro code

--- src/main/java/com/davi/template/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/davi/template/exceptions/handler/GlobalExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.davi.template.exceptions;
 
+import com.davi.template.ErrorCode.ErrorCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -13,31 +13,34 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(SkuNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleSkuNotFoundException(SkuNotFoundException ex) {
-        Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("Error", "Sku Not Found");
-        errorResponse.put("Code", "SKU_NOT_FOUND");
-        errorResponse.put("Description", ex.getMessage());
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        String code = "SKU_NOT_FOUND";
+        String description = ex.getMessage();
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        Map<String, Object> errorResponse = ErrorCode.getErrorResponse(status, code, description);
+
+        return new ResponseEntity<>(errorResponse, status);
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleProductNotFoundException(ProductNotFoundException ex) {
-        Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("Error", "Product Not Found");
-        errorResponse.put("Code", "PRODUCT_NOT_FOUND");
-        errorResponse.put("Description", ex.getMessage());
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        String code = "PRODUCT_NOT_FOUND";
+        String description = ex.getMessage();
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        Map<String, Object> errorResponse = ErrorCode.getErrorResponse(status, code, description);
+
+        return new ResponseEntity<>(errorResponse, status);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleGenericException(Exception ex) {
-        Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("Error", "Internal Server Error");
-        errorResponse.put("Code", "INTERNAL_SERVER_ERROR");
-        errorResponse.put("Description", ex.getMessage());
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String code = "INTERNAL_SERVER_ERROR";
+        String description = ex.getMessage();
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        Map<String, Object> errorResponse = ErrorCode.getErrorResponse(status, code, description);
+
+        return new ResponseEntity<>(errorResponse, status);
     }
 }
